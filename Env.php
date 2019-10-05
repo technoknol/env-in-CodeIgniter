f7ccd761458108a52c702cf927e8a7dbc3f2eec2
Technoknol <technoknol@users.noreply.github.com>
2019-10-05 13:25:33 +0530

    Create Env.php

---
+++ Env.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// file: application/libraries/Env.php 
+class Env
+{
+    public function __construct()
+    {
+        $dotenv = Dotenv\Dotenv::create(FCPATH);
+        $dotenv->load();
+    }
+}

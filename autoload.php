7facf086a694471293c05fc1e394bbd9a1e84bc8
Technoknol <technoknol@users.noreply.github.com>
2019-10-05 13:22:52 +0530

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,4 +1,10 @@
+<?php 
+
 // file : application/config/autoload.php
 // Autoload env library. 
 
 $autoload['libraries'] = array('env');
+
+
+// load general_helper
+$autoload['helper'] = array('general');
